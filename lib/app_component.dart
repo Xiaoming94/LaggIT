4d87e1691c1802a01b0ef4d5d97db2ec72823a13
Sephecure <Sephecure@outlook.com>
2016-12-14 14:15:58 +0100

    Add basic Dart web project structure

---
+++ lib/app_component.dart
@@ -0,0 +1,11 @@
+import 'package:angular2/core.dart';
+
+@Component(
+	selector: 'laggit-app',
+	styleUrls: const ['app_component.css'],
+	templateUrl: 'app_component.html'
+)
+
+class AppComponent {
+	String title = "LaggIT";
+}
