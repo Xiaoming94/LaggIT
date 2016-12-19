900aed4a091194d31c25738978a81dcbe0c0195d
Gurgy <gurgy@emycia.com>
2016-12-19 20:52:56 +0100

    Refactored json image

--- LEvent.go
+++ LEvent.go
@@ -1,21 +1,17 @@
 package main
 
 import (
-	"image"
-	_ "image/gif"
-	_ "image/jpeg"
-	_ "image/png"
 	"time"
 )
 
 type LEvent struct {
-	Title  string      `json:"title"`
-	Time   time.Time   `json:"time"`
-	Place  string      `json:"place"`
-	Price  int         `json:"price"`
-	Info   string      `json:"info"`
-	IsOpen bool        `json:"is_open"`
-	Image  image.Image `json:"image"`
+	Title  string    `json:"title"`
+	Time   time.Time `json:"time"`
+	Place  string    `json:"place"`
+	Price  int       `json:"price"`
+	Info   string    `json:"info"`
+	IsOpen bool      `json:"is_open"`
+	Image  string    `json:"image"` // The source of the image
 }
 
 type Levents []LEvent
--- frontend/lib/laggit_booking/event.dart
+++ frontend/lib/laggit_booking/event.dart
@@ -1,7 +1,9 @@
 import 'dart:html';
 
 int _toInt(i) => i is int ? i : int.parse(i);
-bool _toBool(i) => i;
+bool _toBool(i) => i is bool ? i : i.toLowerCase() == 'true';
+ImageElement _toImage(i) => ImageElement(i, Null, Null);
+DateTime _toTime(i) => DateTime.parse(i);
 
 class LEvent {
   String title;
@@ -16,10 +18,10 @@ class LEvent {
 
   factory LEvent.fromJson(Map<String, dynamic> event) => new LEvent(
       event['title'],
-      DateTime.parse(event['time']),
+      _toTime(event['time']),
       event['place'],
       _toInt(event['price']),
       event['info'],
-      event['image'],
+      _toImage(event['image']),
       _toBool(event['is_open']));
 }
