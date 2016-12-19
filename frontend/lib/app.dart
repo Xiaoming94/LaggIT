4012eb0ecc0c2140ef811fce0082a4243f6e91cc
Gurgy <gurgy@emycia.com>
2016-12-19 09:47:14 +0100

    Rewrote fullpage loading to work with ngfor loops

--- frontend/lib/app.dart
+++ frontend/lib/app.dart
@@ -4,23 +4,24 @@ import 'package:LaggIT/laggit_info/app.dart';
 import 'package:LaggIT/laggit_booking/app.dart';
 import 'package:LaggIT/laggit_contact/app.dart';
 import 'package:LaggIT/laggit_login/app.dart';
+import 'laggit_booking/event_service.dart';
 import 'dart:js' as js;
 
 @Component(
-	selector: 'laggit',
-	styleUrls: const ['app.css'],
-	templateUrl: 'app.html',
-	directives: const [
-		HomeModule,
-		InfoModule,
-		BookingModule,
-		ContactModule,
-		LoginModule
-	],
+  selector: 'laggit',
+  styleUrls: const ['app.css'],
+  templateUrl: 'app.html',
+  directives: const [
+    HomeModule,
+    InfoModule,
+    BookingModule,
+    ContactModule,
+    LoginModule
+  ],
+  providers: const [EventService],
 )
-
 class LaggIT implements AfterContentInit {
-	void ngAfterContentInit() {
-		js.context.callMethod(r'$', ['#fullpage']).callMethod('fullpage', [new js.JsObject.jsify({'scrollingSpeed': 500})]);
-	}
+  void ngAfterContentInit() {
+    js.context.callMethod("loadFullpage");
+  }
 }
