f7f41380bc798185a135de1e6684401ae8e370c2
Gurgy <gurgy@emycia.com>
2016-12-19 17:45:13 +0100

    Expand and refactor event to work with json

--- frontend/lib/laggit_booking/event.dart
+++ frontend/lib/laggit_booking/event.dart
@@ -1,5 +1,8 @@
 import 'dart:html';
 
+int _toInt(i) => i is int ? i : int.parse(i);
+bool _toBool(i) => i;
+
 class LEvent {
   String title;
   DateTime time;
@@ -7,7 +10,16 @@ class LEvent {
   int price;
   String info;
   ImageElement image;
-  bool is_open;
-  LEvent(this.name);
-  LEvent();
+  bool isOpen;
+  LEvent(this.title, this.time, this.place, this.price, this.info, this.image,
+      this.isOpen);
+
+  factory LEvent.fromJson(Map<String, dynamic> event) => new LEvent(
+      event['title'],
+      DateTime.parse(event['time']),
+      event['place'],
+      _toInt(event['price']),
+      event['info'],
+      event['image'],
+      _toBool(event['is_open']));
 }
